9f69e49aa7ba3699e58958807ab7208fb1919d7b
somin <83453597+yygs321@users.noreply.github.com>
2022-09-21 19:21:31 +0900

    Initial commit 초기 설정

---
+++ src/main/java/fitback/fitbackBE/FitbackBeApplication.java
@@ -0,0 +1,13 @@
+package fitback.fitbackBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbackBeApplication.class, args);
+	}
+
+}
