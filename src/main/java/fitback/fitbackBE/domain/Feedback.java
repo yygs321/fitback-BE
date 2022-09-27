7353d114171d0d98e7254c9bef818d3bd2e55215
Tldkt <evolym2543@sookmyung.ac.kr>
2022-09-27 14:12:50 +0900

    [feat] SessionType 수업상태 ABSENT 추가
    정규 수업일에 학생이 지각한 경우 고려해 ABSENT 추가
    기본 상태를 REGULAR로 설정

--- src/main/java/fitback/fitbackBE/domain/Feedback.java
+++ src/main/java/fitback/fitbackBE/domain/Feedback.java
@@ -1,19 +1,38 @@
 package fitback.fitbackBE.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-@Getter
-@Setter
+
+
+@Getter @Setter
+@Entity
+@Table(name="feedback")
+@RequiredArgsConstructor
 public class Feedback {
+
     @Id
+    @GeneratedValue
+    @Column(name="feedback_id")
     private Long id;
+
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name="user_id")
+//    private User user;
+    @Column(length = 50, nullable = false)
     private String title;
+
+    @Column
     private String contents;
-    private SessionType sessionType;//REGULAR, EXTRA
-    private LocalDate createdTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private SessionType sessionType = SessionType.REGULAR;//수업 상태 [REGULAR, ABSENT, EXTRA]
+
+    private LocalDate createdTime;//작성시간
+
+    @Enumerated(EnumType.STRING)
     private PublishType publishType;//OPEN,HIDE
 
 
