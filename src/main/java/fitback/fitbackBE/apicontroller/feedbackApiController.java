3bc1d04134aca08d530b0d65808d249ec8927c5a
Tldkt <evolym2543@sookmyung.ac.kr>
2022-09-27 15:27:39 +0900

    [Feat] 글 수정 기능 patchFeedback() 추가
    postFb의 ResponseDto 방식과 대조하기 위해 ResponseEntity 방식으로 작성

--- src/main/java/fitback/fitbackBE/apicontroller/feedbackApiController.java
+++ src/main/java/fitback/fitbackBE/apicontroller/feedbackApiController.java
@@ -6,8 +6,11 @@ import fitback.fitbackBE.domain.PublishType;
 import fitback.fitbackBE.domain.SessionType;
 import fitback.fitbackBE.mapper.FeedbackMapper;
 import fitback.fitbackBE.service.FeedbackService;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,22 +27,23 @@ public class feedbackApiController {
     private final  FeedbackService feedbackService;
     private final FeedbackMapper mapper;
 
-    public feedbackApiController(FeedbackService feedbackService, FeedbackMapper mapper) {
-        this.feedbackService = feedbackService;
-        this.feedbackMapper = mapper;
-    }
-
 
     @PostMapping
-    public CreatedFeedbackResponse createFeedback(@RequestBody CreatedFeedbackRequest request){
-        Feedback feedback = feedbackService.write(mapper.createRqToFeedBack(request));
+    public CreatedFeedbackResponse createFeedback(@RequestBody CreateFeedbackRequest request){
+        Feedback feedback = feedbackService.write(mapper.mapCreateRqToFeedBack(request));
 
         return new CreatedFeedbackResponse(id);
     }
 
+    @PatchMapping("/{feedback-id}")
+    public ResponseEntity patchFeedback(@PathVariable("feedback-id")@Positive long id, PatchFeedbackRequest request){
+        request.setFbId(id);
+        Feedback feedback = feedbackService.upsateFeedback(mapper.mapPatchFbRqToFeedback(request));
+
+    }
 
     @Data
-    public static class CreatedFeedbackRequest{//static으로 선언할지 고민중
+    public static class CreateFeedbackRequest{
         @NotNull
         private String title;
         private String contents;
@@ -47,10 +51,16 @@ public class feedbackApiController {
         private PublishType publishType;
     }
     @Data
-    public static class CreatedFeedbackResponse {//static으로 선언할지 고민중
+    @Builder
+    @Getter
+    public static class CreatedFeedbackResponse {
         private Long id;
+
         public CreatedFeedbackResponse(Long id){ this.id = id;}
     }
 
 
+    private class PatchFeedbackRequest {
+
+    }
 }
