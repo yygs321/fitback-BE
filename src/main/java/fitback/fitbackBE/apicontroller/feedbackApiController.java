e0c73c437b4e06286948804005cf450217a6b10a
Tldkt <evolym2543@sookmyung.ac.kr>
2022-09-27 14:28:37 +0900

    [Refactor] FeedbackApiController mapper 클래스 활용
    mapper 클래스의 feedbackRq to Feedback() 메서드 틀 작성

--- src/main/java/fitback/fitbackBE/apicontroller/feedbackApiController.java
+++ src/main/java/fitback/fitbackBE/apicontroller/feedbackApiController.java
@@ -1,32 +1,55 @@
 package fitback.fitbackBE.apicontroller;
 
+import com.sun.istack.NotNull;
 import fitback.fitbackBE.domain.Feedback;
+import fitback.fitbackBE.domain.PublishType;
+import fitback.fitbackBE.domain.SessionType;
+import fitback.fitbackBE.mapper.FeedbackMapper;
 import fitback.fitbackBE.service.FeedbackService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+/*피드백 작성
+ * @Date 220925 @Author 정희윤
+ * Dto: CreateFeedbackRequest(title 필수), CreateFeedbackResponse(id)
+ * title 저장, write로 DB저장, id로 변경된 feedbackResponse 조회해 리턴
+ */
+@RestController
+@RequestMapping("/v1/feedback")
+@RequiredArgsConstructor
+@Validated
 public class feedbackApiController {
-    private final FeedbackService feedbackService;
-    /*피드백 작성
-    * @Date 220925 @Author 정희윤
-    * Dto: CreateFeedbackRequest(title 필수), CreateFeedbackResponse(id)
-    * title 저장, write로 DB저장, id로 변경된 feedbackResponse 조회해 리턴
-    */
-    @PostMapping("/v1/feedback")
+    private final  FeedbackService feedbackService;
+    private final FeedbackMapper mapper;
+
+    public feedbackApiController(FeedbackService feedbackService, FeedbackMapper mapper) {
+        this.feedbackService = feedbackService;
+        this.feedbackMapper = mapper;
+    }
+
+
+    @PostMapping
     public CreatedFeedbackResponse createFeedback(@RequestBody CreatedFeedbackRequest request){
-        Feedback feedback = new Feedback();
-        feedback.setTitle(request.getTitle());
-        Long id = feedbackService.write(feedback);
+        Feedback feedback = feedbackService.write(mapper.createRqToFeedBack(request));
+
         return new CreatedFeedbackResponse(id);
     }
 
 
-
-    public class CreatedFeedbackRequest{
+    @Data
+    public static class CreatedFeedbackRequest{//static으로 선언할지 고민중
+        @NotNull
         private String title;
+        private String contents;
+        private SessionType sessionType;
+        private PublishType publishType;
     }
-    private class CreatedFeedbackResponse {
+    @Data
+    public static class CreatedFeedbackResponse {//static으로 선언할지 고민중
         private Long id;
-        public CreateFeedbackResponse(Long id){ this.id = id;}
+        public CreatedFeedbackResponse(Long id){ this.id = id;}
     }
 
 
