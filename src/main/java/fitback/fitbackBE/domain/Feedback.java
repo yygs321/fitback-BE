0ce4877964bfff23e1359f2bef7282f16234bd38
Tldkt <evolym2543@sookmyung.ac.kr>
2022-10-17 16:59:23 +0900

    [implement] getClassRound 메서드 완성

--- src/main/java/fitback/fitbackBE/domain/Feedback.java
+++ src/main/java/fitback/fitbackBE/domain/Feedback.java
@@ -1,4 +1,15 @@
 package fitback.fitbackBE.domain;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class Feedback {
+    private int classRound;
+    public Feedback(int classRound) {
+        this.classRound =classRound;
+    }
+
+    public int getClassRound() {
+        return this.classRound;
+    }
 }
--- src/test/java/fitback/fitbackBE/domain/FeedbackTest.java
+++ src/test/java/fitback/fitbackBE/domain/FeedbackTest.java
@@ -3,16 +3,20 @@ package fitback.fitbackBE.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.*;
+import  static org.assertj.core.api.Assertions.*;
 
 class FeedbackTest {
     @Test
-    public void testFeedback() throws Exception {
+     void testFeedback() throws Exception {
         Feedback feedback = new Feedback();
+        assertNotNull(feedback,"null이 아니어야 합니다.");
     }
-    /*수업회차를 조회하는 기능*/
+
     @Test
     @DisplayName("수업회차 조회기능")
-    public void testGetClassRound() throws Exception{
+     void testGetClassRound() throws Exception{
         //Given
         int classRound =1;
 
@@ -20,7 +24,8 @@ class FeedbackTest {
         Feedback feedback = new Feedback(1);
 
         //then
-        assertEquals(classRound,feedback.getClassRound());
+//        assertEquals(classRound,feedback.getClassRound());
+        assertThat(feedback.getClassRound()).as("check classRound").isEqualTo(classRound);
     }
 
 }
